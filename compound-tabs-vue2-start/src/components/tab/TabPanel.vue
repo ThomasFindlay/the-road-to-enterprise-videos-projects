11f2d3a5ec2d9a5aecc7bfbb004a3f9fff27aaf0
Thomas Findlay <kotowicztom@gmail.com>
2021-03-28 19:03:44 +0100

    commit

---
+++ compound-tabs-vue2-start/src/components/tab/TabPanel.vue
@@ -0,0 +1,19 @@
+<template>
+  <div v-show="tabState.activeTab === id">
+    <slot />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
+  inject: ['tabState'],
+}
+</script>
+
+<style module></style>
